8b7df7f3d2f67607405a429c0840626b652de58a
Evan Hammer <evan@evanhammer.com>
2014-04-23 17:36:20 -0400

    Split application and landing pages sent by Server.

--- writability/controllers/frontend.py
+++ writability/controllers/frontend.py
@@ -7,5 +7,10 @@ bp = Blueprint('frontend', __name__)
 
 
 @bp.route('/')
-def home():
+def landing():
     return render_template('pages/landing.jinja')
+
+
+@bp.route('/home')
+def home():
+    return render_template('pages/app.jinja')
