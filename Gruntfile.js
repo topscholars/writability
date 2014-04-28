209002f053b4d463b5aeae1d54f2bf0d8c9239aa
Evan Hammer <evan@evanhammer.com>
2014-04-28 18:03:20 -0400

    Add concatenate javascript build step.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,10 @@ module.exports = function(grunt) {
                 files: 'writability/static/src/**/*.hbs',
                 tasks: ['emberTemplates']
             },
+            concat: {
+                files: 'writability/static/src/**/*.js',
+                tasks: ['concat']
+            }
         },
         emberTemplates: {
             compile: {
@@ -20,11 +24,22 @@ module.exports = function(grunt) {
                     "writability/static/src/templates.js": "writability/static/src/**/*.hbs"
                 }
             }
+        },
+        concat: {
+            options: {
+            },
+            dist: {
+                // the files to concatenate
+                src: ['writability/static/src/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'writability/static/dist/writability.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-ember-templates');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['emberTemplates']);
+    grunt.registerTask('default', ['emberTemplates', 'concat']);
 };
