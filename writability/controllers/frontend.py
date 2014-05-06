22f14311e81d5089a00840a1e700cda0006f37e7
Evan Hammer <evan@evanhammer.com>
2014-05-06 19:42:21 -0400

    Set up server routing for authenticated user.

--- writability/controllers/frontend.py
+++ writability/controllers/frontend.py
@@ -1,17 +1,22 @@
-# ----------------------------------------------------------------------------#
-# Frontend Controllers
-# ----------------------------------------------------------------------------#
+"""
+controllers.frontend
+~~~~~~~~~~~~~~~~~~~~
+
+This module contains all frontend controllers.
+
+All security controllers are implemented silently through Flask-Security.
+
+"""
 from flask import Blueprint, render_template
+from flask.ext.login import current_user
 
 bp = Blueprint('frontend', __name__)
 
 
-@bp.route('/landing')
-def landing():
-    return render_template('pages/landing.jinja')
-
-
 @bp.route('/', defaults={'path': '/'})
 @bp.route('/<path:path>')
-def home(path):
-    return render_template('pages/app.jinja')
+def index(path):
+    if current_user.is_authenticated():
+        return render_template('pages/app.jinja')
+    else:
+        return render_template('pages/landing.jinja')
