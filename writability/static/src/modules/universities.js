9f99134ff6b2b9e23dd71dd75656230f1be84834
Kirk Portas <kirkportas@gmail.com>
2014-05-18 20:52:41 -0400

    No errors given on convertEssay() handling

--- writability/static/src/modules/universities.js
+++ writability/static/src/modules/universities.js
@@ -41,31 +41,36 @@ App.UniversitiesController = Ember.ArrayController.extend({
                     existing_app_essay_tmp_ids.push(app_essay.essay_template.id);
                 });
             });
+        //console.log(existing_app_essay_tmp_ids);
 
         this.getAllTemplatesForStudent(student)
-            .then( function (app_essay_templates) {
+            .done( function (app_essay_templates) {
                 var all_themes = [];
-                app_essay_templates.forEach(function (item, index) {            // For each AppEssay Template
-                    if (existing_app_essay_tmp_ids.indexOf(item.id) == -1) {    // If related Essay doesn't exist
-                        var item_id = item.get('id');  
-                        var themes_id_obj_dict = [];
+                console.log('all_themes at start: ' + all_themes);
+                app_essay_templates.forEach(function (app_essay_template, index) {            // For each AppEssay Template
+                    if (existing_app_essay_tmp_ids.indexOf(app_essay_template.id) == -1) {    // If related Essay doesn't exist
+                        var item_id = app_essay_template.get('id');  
 
                         // Create App Essay. Setting app_essay during theme_essay creation handles both sides of the relationship
-                        var app_essay_id = that.createAppEssay(student, item);
+                        if ( existing_app_essay_tmp_ids.indexOf(item_id) == -1 ) {
+                            var app_essay_id = that.createAppEssay(student, app_essay_template);
+                            existing_app_essay_tmp_ids.push(item_id);
+                        }
 
                         // Create Theme Essays for each app essay template
-                        item.get('themes').then(function (themes) {             // Each app_ess_tmp hasMany themes
+                        app_essay_template.get('themes').then(function (themes) {       // Each app_ess_tmp hasMany themes
                             var themes_length =  themes.get('length');
 
-                            themes.forEach( function (theme, index) {                      // This theme variable doesn't contain 
+                            themes.forEach( function (theme, index) {                   // This theme variable doesn't contain 
                                 var theme_id = theme.get('id'); 
-                                console.log('theme: ' + theme + 'theme_id: ' + theme_id);                     // Collect data before entering these functions
-                                theme.get('theme_essay_template')                   // TODO: API call here is horrific
-                                    .then(function (theme_essay_template) {         // ERROR: This sometimes gives a NULL error. (theme_id=6)
-                                    //console.log('thm_ess_template:' + theme_essay_template);
-                                        //all_themes.addObject(theme);                  // Ember, adds if does not exist.                    //
-                                        if ( all_themes.indexOf(theme_id) == -1 ) {     // If themeEssay not yet created    
-                                            all_themes.push(theme_id);
+                                if ( all_themes.indexOf(theme_id) == -1 ) {             // If themeEssay not yet created 
+                                    console.log('theme: ' + theme + 'theme_id: ' + theme_id);  // Collect data before entering these functions    
+                                    console.log(all_themes);
+                                    console.log('index_of id: ' + all_themes.indexOf(theme_id));
+                                    all_themes.push(theme_id);
+
+                                    theme.get('theme_essay_template')                   // TODO: API call here is horrific
+                                        .then(function (theme_essay_template) {         // ERROR: This sometimes gives a NULL error. (theme_id=6)
                                             console.log('theme essay_template: ' + theme_essay_template);
                                             var theme_essay = that.store.createRecord('theme_essay', {
                                                 theme: theme,
@@ -73,14 +78,18 @@ App.UniversitiesController = Ember.ArrayController.extend({
                                                 essay_template: theme_essay_template,// get theme essay template
                                                 student: student,
                                                 state: "new",                        // AssertionError without
-                                                proposed_topics: []                  // Without this attr, it tries False gives a Bool isn't iterable error
+                                                proposed_topics: ["",""]                  // Without this attr, it tries False gives a Bool isn't iterable error
                                             });
                                             theme_essay.save();                      // Create theme_essay
-                                        }
-                                        if (index == themes_length - 1) {             // Resolve when complete!
-                                            deferred.resolve();
-                                        }
-                                });
+                                        
+                                            if (index == themes_length - 1) {             // Resolve when complete!
+                                                deferred.resolve();
+                                            }
+                                        })
+                                        .catch( function(error) {
+                                            console.log(theme_id + ' <- If this is "6" then this is an ember quirk where a .get fails');
+                                        }); 
+                                }
                             });
                         });
                     } else {
@@ -137,26 +146,24 @@ App.UniversitiesController = Ember.ArrayController.extend({
         next: function() {
             var that = this;
 
-            this.store.find('student', 0)
-                .then(function (student) {
-                    student.save()
-                        .then( function () { 
-                            that.convertEssays(student)  // Create App & Theme essays from Univs' prompts
-                                .then( function () {
-                                    student.set('state', 'active');             // Set student state to active
-                                    student.save();
-                                    console.log('transitioning to /essays');
-                                    that.transitionToRoute("essays");           // Redirect to Essays page
-                                })
-                                .catch( function (error) {
-                                    console.log(error);
-                                });                
-                        })
-                        .catch( function (error) { 
-                            console.log(error);
-                            alert("Sorry! We've encountered an error."); 
-                        });
-                });
+            this.store.find('student', 0).then(function (student) {
+                student.save()
+                    .then( function () { 
+                        that.convertEssays(student)  // Create App & Theme essays from Univs' prompts
+                            .done( function () {
+                                student.set('state', 'active');             // Set student state to active
+                                student.save();
+                                that.transitionToRoute("essays");           // Redirect to Essays page
+                            })
+                            .fail( function (error) {
+                                console.log(error);
+                            });                
+                    })
+                    .catch( function (error) { 
+                        console.log(error);
+                        alert("Sorry! We've encountered an error."); 
+                    });
+            });
         }
     }
 });
