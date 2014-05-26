789787009881b3b37a4c403631973024b758b30f
Kirk Portas <kirkportas@gmail.com>
2014-05-26 15:16:34 -0400

    Fix jasmine setup, now working

--- writability/spec/javascripts/helpers/spec_helper.js
+++ writability/spec/javascripts/helpers/spec_helper.js
@@ -1,14 +1,14 @@
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-document.write('<style>#ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
+//document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+//document.write('<style>#ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
 
-Ember.testing = true;
+//Ember.testing = true;
 
 // Creating an application normally happens async,
 // which is why we have to wrap it in Ember.run
-Ember.run(function() {
-  App = Ember.Application.create();
-});
-App.rootElement = '#ember-testing';
+//Ember.run(function() {
+//  App = Ember.Application.create();
+//});
+App.rootElement = '#application-root';
 App.setupForTesting();
 App.injectTestHelpers();
 
