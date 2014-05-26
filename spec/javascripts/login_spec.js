277f79cd196c50e010b55531a36634de7a1b9ff9
Kirk Portas <kirkportas@gmail.com>
2014-05-26 16:43:20 -0400

    WIP login spec

--- spec/javascripts/login_spec.js
+++ spec/javascripts/login_spec.js
@@ -6,19 +6,62 @@ describe('App.IndexRoute', function() {
 
     route = App.IndexRoute.create();
     spyOn(route, 'controllerFor').and.returnValue(controller);
+  
+    //For view tests
+    root = $('body').append('<div id="view-tests" />');
+    rootElement = window.App.rootElement;
+    window.App.rootElement = $("#view-tests");
+  });
+  afterEach(function() {
+      Ember.run(function() {
+          window.App.rootElement = rootElement;
+          $("#view-tests").html('');
+      });
   });
 
-  it("calls controllerFor with application", function() {
-    Ember.run(function() {
-      route.activate();
-    });
-    //route.activate();
+  it ("calls controllerFor with application", function() {
+    //Ember.run(function() {
+    //  route.activate();
+    //});
+    route.activate();
     //expect(route.controllerFor).toHaveBeenCalledWith('application');
   });
 
+  it ("opens landing page", function() {
+    visit("/");
+
+    //browser().navigateTo('#/');
+    //expect(browser().location().path()).toBe("/login");
+    //page.should have_content("login");
+
+    //   done();
+  });
+
+
+  it ("arbitrary template will render given output", function(){
+
+
+
+        var view = Ember.View.create({
+            template: Ember.Handlebars.compile('bar')
+        });
+        Ember.run(function() {
+            view.appendTo("#view-tests");
+        });
+        expect(view.$().text()).toEqual("bar");
+
+        // move to after each
+        Ember.run(function() {
+            window.App.rootElement = rootElement;
+            $("#view-tests").html('');
+        });
+    });
+
+
+
   xit("sets loginLayout on applicationController on activate", function() {
     route.activate(); 
-    expect(controller.set).toHaveBeenCalledWith('loginLayout', true);
+    expect(controller.set).toHaveBeenCalledWith('ApplicationController', true);
   });
 
   xit("unsets loginLayout on applicationController on deactivate", function() {
