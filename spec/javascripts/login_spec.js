c350d88903b86ce6e5098a22759ef758ce74815d
Kirk Portas <kirkportas@gmail.com>
2014-05-26 17:36:57 -0400

    Add karma, settings, and WIP on specs. Karma failing on 'App.reset()'

--- spec/javascripts/login_spec.js
+++ spec/javascripts/login_spec.js
@@ -1,12 +1,6 @@
-describe('App.IndexRoute', function() {
-  var controller, route;
-
-  beforeEach(function() {
-    controller = jasmine.createSpyObj('controller', ['set']);
-
-    route = App.IndexRoute.create();
-    spyOn(route, 'controllerFor').and.returnValue(controller);
-  
+describe('user login', function() {
+   beforeEach(function() {
+    //route = App.IndexRoute.create();
     //For view tests
     root = $('body').append('<div id="view-tests" />');
     rootElement = window.App.rootElement;
@@ -19,53 +13,13 @@ describe('App.IndexRoute', function() {
       });
   });
 
-  it ("calls controllerFor with application", function() {
-    //Ember.run(function() {
-    //  route.activate();
-    //});
-    route.activate();
-    //expect(route.controllerFor).toHaveBeenCalledWith('application');
+  it('ensures user can log in', function() {
+
+    browser().navigateTo('/');
+    // expect current scope to contain username
+  });
+  it('ensures path has changed', function() {
+    // expect path to equal '/dashboard'
   });
 
-  it ("opens landing page", function() {
-    visit("/");
-
-    //browser().navigateTo('#/');
-    //expect(browser().location().path()).toBe("/login");
-    //page.should have_content("login");
-
-    //   done();
-  });
-
-
-  it ("arbitrary template will render given output", function(){
-
-
-
-        var view = Ember.View.create({
-            template: Ember.Handlebars.compile('bar')
-        });
-        Ember.run(function() {
-            view.appendTo("#view-tests");
-        });
-        expect(view.$().text()).toEqual("bar");
-
-        // move to after each
-        Ember.run(function() {
-            window.App.rootElement = rootElement;
-            $("#view-tests").html('');
-        });
-    });
-
-
-
-  xit("sets loginLayout on applicationController on activate", function() {
-    route.activate(); 
-    expect(controller.set).toHaveBeenCalledWith('ApplicationController', true);
-  });
-
-  xit("unsets loginLayout on applicationController on deactivate", function() {
-    route.deactivate();
-    expect(controller.set).toHaveBeenCalledWith('loginLayout', false);
-  });
 });
