f6b7322ce033c2311dbfa5deb4a63b24436d5277
Ryan Tablada <ryan.tablada@gmail.com>
2014-06-04 11:52:53 -0500

    Add validatorJS

---
+++ writability/static/libs/validatorjs/spec/alpha-rule.spec.js
@@ -0,0 +1,41 @@
+// for jasmine-node support
+if (typeof process !== 'undefined' && process.title && process.title === 'node') { // detect node environment
+	var Validator = require('./../src/validator');
+}
+
+// only checks numeric, string, and undefined
+
+describe('alpha validation rule', function() {
+	var validator;
+	
+	it('should fail with non-alphabetic characters', function() {
+		validator = new Validator({ name: '12' }, { name: 'alpha' });
+		expect(validator.fails()).toBeTruthy();
+		expect(validator.passes()).toBeFalsy();
+	});
+
+	it('should fail with non-alphabetic characters', function() {
+		validator = new Validator({ name: 12 }, { name: 'alpha' });
+		expect(validator.fails()).toBeTruthy();
+		expect(validator.passes()).toBeFalsy();
+	});
+
+	it('should pass with only alphabetic characters', function() {
+		validator = new Validator({ name: 'abc' }, { name: 'alpha' });
+		expect(validator.fails()).toBeFalsy();
+		expect(validator.passes()).toBeTruthy();
+	});
+
+
+
+	it('should pass when the field is an empty string', function() {
+		var validator = new Validator({ name: '' }, { name: 'alpha' });
+		expect(validator.passes()).toBeTruthy();
+	});
+
+	it('should pass when the field does not exist', function() {
+		var validator = new Validator({}, { name: 'alpha' });
+		expect(validator.passes()).toBeTruthy();
+		expect(validator.fails()).toBeFalsy();
+	});
+});
