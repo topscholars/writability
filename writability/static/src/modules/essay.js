3d3b9acab978f06c933a336cce0f86e40f62f9d8
Ryan Tablada <ryan.tablada@gmail.com>
2014-06-04 12:04:50 -0500

    Basic submit validation

--- writability/static/src/modules/essay.js
+++ writability/static/src/modules/essay.js
@@ -2,6 +2,11 @@
 App.EssayController = Ember.ObjectController.extend({
     needs: ['essays'],
 
+    proposedTopicsRules: {
+        'proposed_topic_0': 'required',
+        'proposed_topic_1': 'required'
+    },
+
     currentDraft: function () {
         return this.draftByMostCurrent(0);
     }.property('drafts'),
@@ -86,6 +91,20 @@ App.EssayController = Ember.ObjectController.extend({
             this.getMostRecentDraft().then(function (id) {
                 that.transitionToRoute('draft', id);
             });
+        },
+        submitProposedTopics: function(model) {
+            var input = {
+                proposed_topic_0: model.get('proposed_topic_0'),
+                proposed_topic_1: model.get('proposed_topic_1')
+            };
+            var validator = new Validator(input, this.proposedTopicsRules);
+            if (validator.fails()) {
+                alert('You must supply two proposed topics');
+            } else {
+                if (confirm('Are you sure you want to submit these topics?')) {
+                    console.log('submitting');
+                }
+            }
         }
     }
 });
