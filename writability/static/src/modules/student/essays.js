b99b698046c6c00b1b6cb329fd1928e4a336999b
Ryan Tablada <ryan.tablada@gmail.com>
2014-06-06 17:11:22 -0500

    Select an essay

--- writability/static/src/modules/student/essays.js
+++ writability/static/src/modules/student/essays.js
@@ -1,17 +1,22 @@
 App.StudentEssaysController = Ember.ArrayController.extend({
-	needs: ['student'],
-	student: Ember.computed.alias('controllers.student.model'),
-	actionRequiredEssays: Ember.computed.filter('model', function(essay) {
-		return essay.state != 'completed';
-	})
+    needs: ['student'],
+    student: Ember.computed.alias('controllers.student.model'),
+    actionRequiredEssays: Ember.computed.filter('model', function(essay) {
+        return essay.state != 'completed';
+    }),
+    actions: {
+        select: function() {
+            console.log('select essay');
+        }
+    }
 });
 
 App.StudentEssaysHeaderView = Ember.View.extend({
-	templateName: 'modules/student/essay'
+    templateName: 'modules/student/essay'
 });
 
 App.StudentEssaysView = App.ListView.extend({
-	templateName: 'modules/student/essay-layout',
+    templateName: 'modules/student/essay-layout',
     //sections: ['To do', 'Not to do'],
     listItem: App.EssayItemView
 });
