6e387dd3902af80e012ed42066a6210c8b5afdb8
Ryan Tablada <ryan.tablada@gmail.com>
2014-06-06 17:18:28 -0500

    Controllers, Views and add select prompt to StudentEssays

--- writability/static/src/router.js
+++ writability/static/src/router.js
@@ -233,6 +233,7 @@ App.StudentEssaysRoute = App.AuthenticatedRoute.extend({
         // this.render('core/layouts/main');
         this.render('Header', {outlet: 'header'});
         this.render({into: 'core/layouts/main', outlet: 'left-side-outlet'});
+        this.render('core/select-prompt', {into: 'core/layouts/main', outlet: 'right-side-outlet'});
     }
 });
 App.StudentEssayRoute = App.AuthenticatedRoute.extend({
