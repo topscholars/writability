4fbba36ef976a10d5fd65d388c77f2562c40336e
Chris Clark <chrisclark@VirtualBox-13.10>
2014-06-11 16:14:34 -0500

    Add ThemeEssays in add-universities

--- writability/controllers/resource/add_universities.py
+++ writability/controllers/resource/add_universities.py
@@ -3,7 +3,7 @@ from flask import request
 from flask.ext.restful import Resource
 from .user import User
 from models.db import db
-from models.essay import ApplicationEssay
+from models.essay import ApplicationEssay, ThemeEssay
 from models.essay_template import ApplicationEssayTemplate
 
 
@@ -16,20 +16,23 @@ class AddUniversitiesResource(Resource):
     def post(self, student_id):
         university_ids = json.loads(request.form.get('universities', '[]'))
         student = User.query.filter_by(id=student_id).first()
-        required_application_essay_template_ids = []
+        required_application_essay_templates = []
         for university_id in university_ids:
-            application_essay_templates = ApplicationEssayTemplate.query.filter_by(university_id=university_id)
-            required_application_essay_template_ids.extend([x.id for x in application_essay_templates])
+            required_application_essay_templates.extend(
+                ApplicationEssayTemplate.query.filter_by(university_id=university_id))
         existing_application_essay_template_ids = [application_essay.essay_template_id
             for application_essay in student.application_essays]
-        application_essay_template_ids = (set(required_application_essay_template_ids)
-                                        - set(existing_application_essay_template_ids))
-        for application_essay_template_id in application_essay_template_ids:
-            application_essay = ApplicationEssay(essay_template_id=application_essay_template_id,
-                                                 student_id=student.id,
-                                                 essay_prompt='NORMALIZE?') # prompt is already in the template...
+        application_essay_templates = [x for x in required_application_essay_templates
+                                       if x.id not in existing_application_essay_template_ids]
+        for application_essay_template in application_essay_templates:
+            application_essay = ApplicationEssay(essay_template=application_essay_template,
+                                                 essay_prompt=application_essay_template.essay_prompt,
+                                                 student_id=student.id)
             db.session.add(application_essay)
-            # TODO: Foreach theme in the ApplicationEssay, create a ThemeEssay
+            for theme in application_essay_template.themes:
+                theme_essay = ThemeEssay(theme_id=theme.id,
+                                         essay_prompt=application_essay_template.essay_prompt,
+                                         student_id=student.id)
+                db.session.add(theme_essay)
             db.session.commit()
-            pass
-        return 'OK ' + str(application_essay_template_ids)
+        return 'OK ' + str(len(application_essay_templates))
