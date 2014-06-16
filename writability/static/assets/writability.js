e1e2b35973ed26f80173a3adab8282a7babca570
Ryan Tablada <ryan.tablada@gmail.com>
2014-06-16 07:37:09 -0400

    Toggle off merged essays

--- writability/static/assets/writability.js
+++ writability/static/assets/writability.js
@@ -1180,8 +1180,15 @@ App.StudentEssaysShowMergeController = Ember.Controller.extend({
 		},
 		toggleMergeSelected: function(essay) {
 			var mergedEssays = this.get('parentEssay.merged_theme_essays');
-			// Strange but needed to fire listener events for now...
-			this.set('parentEssay.merged_theme_essays', mergedEssays.concat([essay.id]));
+			var indexOf = mergedEssays.indexOf(essay.id);
+
+			if (indexOf === -1) {
+				// Strange but needed to fire listener events for now...
+				this.set('parentEssay.merged_theme_essays', mergedEssays.concat([essay.id]));
+			} else {
+				this.set('parentEssay.merged_theme_essays', mergedEssays.splice(indexOf + 1, 1));
+			}
+			console.log(this.get('parentEssay.merged_theme_essays'));
 		}
 	}
 })
