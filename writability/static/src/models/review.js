f3e86f8e3246476394a3f480e1203987b901be8e
Ryan Tablada <ryan.tablada@gmail.com>
2014-07-10 17:18:08 -0500

    Annotations issue when annotations[0] does not exist

--- writability/static/src/models/review.js
+++ writability/static/src/models/review.js
@@ -13,7 +13,6 @@ App.Review = DS.Model.extend({
     all_annotations_resolved: function() {
         var annotations = this._data.annotations;
         var annos = this.get('annotations');
-        console.log("annotations[0].get('state'): " + annotations[0].get('state') );
 
         for (i=0; i < annotations.length; i++) {
             var anno_state = annotations[i].get('state');
@@ -34,12 +33,12 @@ App.Review = DS.Model.extend({
         //    debugger
             //console.log(anno._data.state);
         //    if ( anno._data.state == "new") {
-        //        return false;                 
+        //        return false;
         //    }
         //}
 
         //this.get('annotations')                   // Same problem without the .then() for async
-        //  .then(function(annotations) { 
+        //  .then(function(annotations) {
         //    for (anno in annotations) {
         //        if ( anno.get('state') == "new") {  // annos is an Ember obj of ~130 elements instead of 4
         //            return false;                   // so can't use != "resolved" .. goddamnit ember
