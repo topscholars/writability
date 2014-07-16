bb74471affd9bb93b62a8b0a6b1f321ccdae281c
Ryan Tablada <ryan.tablada@gmail.com>
2014-07-16 09:09:37 -0500

    All of the requirements and live reload for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,28 @@ var gulp = require('gulp'),
 	es6transpiler = require('gulp-es6-module-transpiler'),
 	concat = require('gulp-concat')
 	handlebars = require('gulp-ember-handlebars'),
-	merge = require('merge-stream');
+	merge = require('merge-stream'),
+	livereload = require('gulp-livereload');
 
 var bowerDir = './writability/static/libs/',
 	dest = gulp.dest('./writability/static/assets');
 
 var vendorFiles = [
-	bowerDir + 'loader.js/loader.js',
+	bowerDir + 'loader/loader.js',
+	bowerDir + 'lodash/dist/lodash.compat.js',
 	bowerDir + 'jquery/dist/jquery.js',
 	bowerDir + 'handlebars/handlebars.js',
 	bowerDir + 'ember/ember.js',
-	bowerDir + 'ember-resolver/dist/ember-resolver.js'
+	bowerDir + 'ember-resolver/dist/ember-resolver.js',
+
+	bowerDir + 'ember-data/ember-data.js',
+	bowerDir + 'bootstrap/dist/js/bootstrap.js',
+	bowerDir + 'momentjs/moment.js',
+	bowerDir + 'validatorjs/dist/validator.min.js',
+	bowerDir + 'select2/select2.js',
+	bowerDir + 'jquery-ui/js/jquery-ui-1.10.4.custom.js',
+	bowerDir + 'pickadate/lib/picker.js',
+	bowerDir + 'pickadate/lib/picker.date.js'
 ];
 
 gulp.task('vendor', function() {
@@ -35,9 +46,11 @@ gulp.task('ember-app', function() {
 
 	return merge(app, templates)
 		.pipe(concat('app-new.js'))
-		.pipe(dest);
+		.pipe(dest)
+		.pipe(livereload({ auto: false }));;
 });
 
 gulp.task('watch', function () {
+	livereload.listen();
 	gulp.watch(['./writability/ember/**/*.js', './writability/ember/templates/**/*.hbs'], ['ember-app']);
 });
