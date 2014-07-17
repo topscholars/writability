e62902cb03dba30a0f6154daaa3c2477a52dc193
Ryan Tablada <ryan.tablada@gmail.com>
2014-07-17 17:19:14 -0500

    Teacher Draft stuff

--- writability/ember/app/views/draft/summary.js
+++ writability/ember/app/views/draft/summary.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
+import DraftDetails from './summary/details';
+import DraftReview from './summary/review';
+import DraftSettings from './summary/settings';
 
 export default Ember.ContainerView.extend({
     classNames: ['summary-panel'],
@@ -6,15 +9,9 @@ export default Ember.ContainerView.extend({
     activePanel: null,
 
     init: function () {
-        this.set('details', Ember.View.create({
-            templateName: "modules/_draft-details-panel"
-        }));
-        this.set('review', Ember.View.create({
-            templateName: "modules/_draft-review-panel"
-        }));
-        this.set('settings', Ember.View.create({
-            templateName: "modules/draft/_settings-panel"
-        }));
+        this.set('details', DraftDetails.create());
+        this.set('review', DraftReview.create());
+        this.set('settings', DraftSettings.create());
         this.set('childViews', []);
         this._super();
     },
