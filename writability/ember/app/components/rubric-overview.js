82c68fdf0aef6006d39232c28798dde473a7057d
Ryan Tablada <ryan.tablada@gmail.com>
2014-07-22 23:51:10 -0500

    Select Category

--- writability/ember/app/components/rubric-overview.js
+++ writability/ember/app/components/rubric-overview.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	selectedRubricCategory: null,
 
-	rubricCategories: []
+	rubricCategories: [],
+
+	actions: {
+		selectCategory: function (category) {
+			this.set('selectedRubricCategory', category);
+		}
+	}
 });
--- writability/ember/app/components/rubric-slider.js
+++ writability/ember/app/components/rubric-slider.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['rubric-slider'],
+	classNameBindings: ['selected'],
+
 	category: null,
 	selectedRubricCategory: null,
+
 	barWidth: function() {
 		return "width: " + this.get('category.score') + "%;";
 	}.property('category.score'),
+
+	selected: function () {
+		return this.get('category.id') === this.get('selectedRubricCategory.id');
+	}.property('category', 'selectedRubricCategory'),
 
 	actions: {
 		decrement: function () {
@@ -17,6 +25,9 @@ export default Ember.Component.extend({
 			if (this.get('category.score') <= 90) {
 				this.incrementProperty('category.score', 10);
 			}
+		},
+		select: function () {
+			this.sendAction('select', this.get('category'));
 		}
 	}
 });
