2c430332f697fb86ce0e876c60572c9fcf8f40b2
Ryan Tablada <ryan.tablada@gmail.com>
2014-07-22 23:31:27 -0500

    Increment and further styling

--- writability/ember/app/components/rubric-slider.js
+++ writability/ember/app/components/rubric-slider.js
@@ -5,5 +5,18 @@ export default Ember.Component.extend({
 	selectedRubricCategory: null,
 	barWidth: function() {
 		return "width: " + this.get('category.score') + "%;";
-	}.property('category.score')
+	}.property('category.score'),
+
+	actions: {
+		decrement: function () {
+			if (this.get('category.score') >= 10) {
+				this.decrementProperty('category.score', 10);
+			}
+		},
+		increment: function () {
+			if (this.get('category.score') <= 90) {
+				this.incrementProperty('category.score', 10);
+			}
+		}
+	}
 });
