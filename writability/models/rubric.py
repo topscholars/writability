4a8ec55347ffe8b272f90c5db585fb21e7374930
Kirk Portas <kirkportas@gmail.com>
2014-07-23 17:36:41 -0400

    Setup rubric models, with join model including 'grade' for criteria
    Finish rubric models

--- writability/models/rubric.py
+++ writability/models/rubric.py
@@ -5,12 +5,15 @@ models.rubric
 This module contains a Rubric that is used to grade Drafts (as part of a Review).
 
 """
+from sqlalchemy.orm import validates #, Column, Integer, String
+
 from .db import db
 from .base import BaseModel
 # from sqlalchemy.orm import validates
 from annotation import Tag
 
 class Rubric(BaseModel):
+    __tablename__ = 'rubric'
     ## Belongs to REVIEW
     ## When review is submitted, this is copied to next draft
     ## along with current review. (not done)
@@ -20,11 +23,15 @@ class Rubric(BaseModel):
 
     # required fields
     id = db.Column(db.Integer, primary_key=True)
-    categories = db.relationship(
-        "RubricCategory",
-        backref=db.backref("rubrics", uselist=False),
-        uselist=True,
-        nullable=False) # Add category_grade for this. See app_essays association
+    #rubric_categories = db.relationship(
+    #    "RubricCategory",
+    #    backref=db.backref("rubric", uselist=False),
+    #    uselist=True) 
+
+
+    _rubric_categories = db.relationship(
+        "RubricCategoryRubricAssociations",
+        backref=db.backref("rubric"))
 
     # optional fields
     name = db.Column(db.String)
@@ -33,6 +40,7 @@ class Rubric(BaseModel):
     review_id = db.Column(db.Integer, db.ForeignKey("review.id"))
 
 class RubricCategory(BaseModel): # Impact, Content, and Quality.
+    __tablename__ = 'rubric_category'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -41,18 +49,16 @@ class RubricCategory(BaseModel): # Impact, Content, and Quality.
     criteria = db.relationship(
         "Criterion",
         backref=db.backref("rubriccategory", uselist=False),
-        uselist=True,
-        nullable=False)
+        uselist=True)
 
     help_text = db.Column(db.String)
 
 class Criterion(Tag):   # Tags that a teacher can write an annotation against, 
                         # that are also associated with a rubric category
-    pass
-    # rubriccategory: don't explicitly declare it but it's here
-
+    #__tablename__ = 'criteria'
     ##### ADD relationship to RubricCategory 
-    
+    rubric_category_id = db.Column(db.Integer, db.ForeignKey('rubric_category.id'))
+
     ## Creates rubric categories from criteria
     def change_related_objects(self):
         super(Criterion, self).change_related_objects()
@@ -63,4 +69,23 @@ class Criterion(Tag):   # Tags that a teacher can write an annotation against,
             new_rc = RubricCategory(**new_rc_params)
         self.rubriccategory = RubricCategory.read_by_filter({'name':self.name})[0]
 
+class RubricCategoryRubricAssociations(BaseModel):
+    __tablename__ = 'rubric_rubric_category_associations'
+    
+    ALLOWED_GRADES = [0,10,20,30,40,50,60,70,80,90,100]
 
+    @validates('grade')
+    def validate_grades(self, key, grade):
+        """Assert that grade is rounded to 10. 0/10/20../90/100."""
+        assert val in self.ALLOWED_GRADES
+        return grade
+
+    rubric_id = db.Column(
+        db.Integer,
+        db.ForeignKey("rubric.id"),
+        primary_key=True)
+    rubric_category_id = db.Column(
+        db.Integer,
+        db.ForeignKey("rubric_category.id"),
+        primary_key=True)
+    grade = db.Column(db.Integer, default=0)
