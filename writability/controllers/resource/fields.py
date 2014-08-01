bc1e9a7324578e3272c5a79c498a07bc52b19084
Michael Samoylov <michael.samoylov@gmail.com>
2014-08-01 19:33:14 +0400

    changed application_essays format for /api/theme-essays

--- writability/controllers/resource/fields.py
+++ writability/controllers/resource/fields.py
@@ -44,16 +44,15 @@ class ApplicationEssayResourceField(fields.Url):
         if isinstance(obj, Iterable):  # if object is a list get the right key
             ae_id = obj[key].id
             for ea in obj[key].essay_associations:
-                state = ea.theme_essay.state   # TODO: (Mike) discuss it with John
+                state = ea.state   # TODO: (Mike) discuss it with John
         else:  # else, just grab the id from the object
             sub_obj = getattr(obj, key)
             if sub_obj:
                 ae_id = sub_obj.id
                 for ea in sub_obj.essay_associations:
-                    state = ea.theme_essay.state   # TODO: (Mike) discuss it with John
+                    state = ea.state   # TODO: (Mike) discuss it with John
         if ae_id:
-            key = 'appessay_id_%d' % ae_id
-            return {key: state}
+            return dict(application_essay_id=ae_id, state=state)
         else:
             return None
 
