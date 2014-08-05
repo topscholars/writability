565df43c2b15ec36382580f699293ba8c00e7231
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-05 18:31:16 -0500

    List showing up

--- writability/ember/app/models/application-essay.js
+++ writability/ember/app/models/application-essay.js
@@ -5,6 +5,7 @@ export default Essay.extend({
     // properties
 
     // relationships
+    university_name: DS.attr(),
     theme_essays: DS.hasMany('themeEssay', {async: true}),
     essay_template: DS.belongsTo('applicationEssayTemplate', {async: true}),
     essayType: 'application'
