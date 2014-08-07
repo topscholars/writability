5f5152064228d36db16d7bd3f2b5d450502ea616
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-07 11:13:48 -0500

    Draft serialization

--- writability/ember/app/serializers/draft.js
+++ writability/ember/app/serializers/draft.js
@@ -8,6 +8,13 @@ export default ApplicationSerializer.extend({
 
         return this._super(type, hash, prop);
     },
+    serialize: function(record, options) {
+    	var data = this._super(record, options);
+    	data.essay = data.essay_id;
+    	delete data.essay_id;
+
+    	return data;
+    },
     attrs: {
         essay: {deserialize: 'no'}
     }
