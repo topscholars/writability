6e4bb4836820a69ee613f436f0eb2ecee0eec058
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-11 15:57:44 -0500

    Fixes student submitting application essay state

--- writability/ember/app/controllers/application-essay.js
+++ writability/ember/app/controllers/application-essay.js
@@ -1,4 +1,16 @@
 import EssayController from './essay';
 
 export default EssayController.extend({
+	actions: {
+	    openDraft: function () {
+	    	var essay = this.get('model');
+
+	    	if (essay.get('state') === 'new') {
+	    		essay.set('state', 'in_progress');
+	    		essay.save();
+	    	}
+
+	        this._super();
+	    }
+	}
 });
--- writability/ember/app/serializers/application-essay.js
+++ writability/ember/app/serializers/application-essay.js
@@ -5,6 +5,6 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
     attrs: {
         theme_essays: {serialize: 'no'},
-        draft: {serialize: 'no'}
+        drafts: {serialize: 'no'}
     }
 });
