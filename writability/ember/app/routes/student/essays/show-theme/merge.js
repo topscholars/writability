3a1e35ae520059da6af1c73e048926fe757ce6ef
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-25 08:48:12 -0500

    Show only is_displayed theme essays in merge screen

--- writability/ember/app/routes/student/essays/show-theme/merge.js
+++ writability/ember/app/routes/student/essays/show-theme/merge.js
@@ -1,9 +1,13 @@
 import AuthenticatedRoute from 'writability/routes/authenticated';
 
 export default AuthenticatedRoute.extend({
+	model: function() {
+		return this.modelFor('student').get('theme_essays');
+	},
+
     setupController: function(controller, model) {
         controller.set('parentEssay', this.modelFor('student.essays.show-theme'));
-        controller.set('essays', this.modelFor('student.essays'));
+        controller.set('essays', model);
     },
     renderTemplate: function() {
         this.render({into: 'application', outlet: 'modal-module'});
