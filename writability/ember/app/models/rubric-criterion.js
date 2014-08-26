e2919555ee4f47ef61e477c06540974eb3c4579e
Kirk Portas <kirkportas@gmail.com>
2014-08-26 17:13:59 -0400

    WIP display review criteria by rubriccategory and draft's essay template. Error on dotwalking to criteria model in Ember #156

--- writability/ember/app/models/rubric-criterion.js
+++ writability/ember/app/models/rubric-criterion.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 //import FixtureData from 'writability/fixtures/rubric-criterion';
 
 var Criterion = DS.Model.extend({
-	description: DS.attr()
-  //rubric_category: DS.attr()
+	description: DS.attr(),
+  rubriccategory: DS.belongsTo('rubric-category', {async: true})
 });
 
 //RubricCategory.reopenClass({
--- writability/ember/app/models/rubric.js
+++ writability/ember/app/models/rubric.js
@@ -1,10 +1,41 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 //import FixtureData from 'writability/fixtures/rubric';
 
 var Rubric = DS.Model.extend({
 	review: DS.belongsTo('review'),
 	name: DS.attr(),
-	rubric_associations: DS.hasMany('rubric-association')//, {async: true})
+	rubric_associations: DS.hasMany('rubric-association'),//, {async: true})
+
+  essay_template: DS.belongsTo('essay-template'), //only for below alias
+  all_criteria: Ember.computed.alias('essay_template.criteria'),
+
+  impact_criteria: function () {
+    var all_criteria = this.get('all_criteria');
+    var impact_criteria = [];
+    // content, impact, quality, style
+    all_criteria.forEach (function(crit) {
+      var category_name = '';
+      crit.get('rubriccategory')
+        .then( function (category) {
+          debugger
+          category_name = category.name.toLowerCase();
+          if (category_name == "impact") {
+            impact_criteria.push(crit);
+          }
+        });
+    });
+
+  }.property('all_criteria')
+    //var impact_crit = all_criteria.filterBy('')
+    //return 
+  
+
+  //  selectedEssays: function() {
+  //  return this.get('content').filterBy('onboarding_is_selected', true);
+  //}.property('content.@each.onboarding_is_selected'),
+
+
 });
 
 //Rubric.reopenClass({
