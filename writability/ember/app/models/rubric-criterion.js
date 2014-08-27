487a5d2145b920d87b7adfa0ae04e391f5666c2b
Kirk Portas <kirkportas@gmail.com>
2014-08-27 19:01:19 -0400

    WIP further attempts on rubric criteria loading. #156

--- writability/ember/app/models/rubric-criterion.js
+++ writability/ember/app/models/rubric-criterion.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
+//import Ember from 'ember';
 //import FixtureData from 'writability/fixtures/rubric-criterion';
 
 var Criterion = DS.Model.extend({
 	description: DS.attr(),
-  rubriccategory: DS.belongsTo('rubric-category')
+  rubriccategory: DS.belongsTo('rubric-category'),
+  //name: Ember.computed.alias('rubric_category.name')
 });
 
 //RubricCategory.reopenClass({
--- writability/ember/app/models/rubric.js
+++ writability/ember/app/models/rubric.js
@@ -8,43 +8,66 @@ var Rubric = DS.Model.extend({
 	rubric_associations: DS.hasMany('rubric-association'),//, {async: true})
 
   essay_template: DS.belongsTo('essay-template'), //only for below alias
-  all_criteria: Ember.computed.alias('essay_template.criteria'),
+  all_criteria: Ember.computed.alias('essay_template.criteria'), // [1, 2 ,3]
+  // filter 2 by "impact'"
 
-  impact_criteria: function (key, value) {
-    if (arguments.length > 1) { // > 1 args = this is a `set`
-      return value;
-    } else {
+
+  //impact_criteria: function (key, value) {
+  //  if (arguments.length > 1) { // > 1 args = this is a `set`
+  //    return value;
+  //  } else {
+
+  impact_criteria: function () {
       var model = this;
       var all_criteria = this.get('all_criteria');
       var impact_criteria = [];
       // content, impact, quality, style
-      //var peters = this.store.find('criteria', { id: crit.id, name: "Impact" });
-      var all_criteria_length = all_criteria.length;
+      var a = model.store.all('rubric-criterion'); // , { id: "454" })
+      var b = model.store.all('criterion');
       debugger
-      all_criteria.forEach (function(crit, index) {
-        var crit_id = crit.id;
-        model.store.find('rubric-criterion', crit_id )
-          .then( function (crit_obj) {
-            var category_name = crit_obj.get('rubriccategory.name').toLowerCase();
-            if (category_name == "impact") {
-              impact_criteria.push(crit);
-            }
-            debugger
-            if ((index+1) == all_criteria_length) {
-              model.set("impact_criteria", impact_criteria);
-              debugger
-            }
-          });
-        
-      });
+       // .then( function (y) { 
+       //   debugger
+       // });
+      //model.store.find('rubric-category', { name: "Impact"} )
+      //  .then( function (rubric_cat) { 
+      //    model.store.find('rubric-criterion', { essay_template_id: "454" }) //model.get('essay_template.id') } ) //, rubriccategory: rubric_cat.id })
+      //      .then( function (our_beloved_cats) {
+      //        debugger
+      //      });
+      //    
+      //  } );
+      return impact_criteria;
+      
+      //var peters2 = this.store.find('rubric-criterion', { id: crit.id, rubriccategory: "2" });
+
+       
+      //var all_criteria_length = all_criteria.length;
+      ////debugger
+      //all_criteria.forEach (function(crit, index) {
+      //  var crit_id = crit.id;
+      //  model.store.find('rubric-criterion', crit_id )
+      //    .then( function (crit_obj) {
+      //      var category_name = crit_obj.get('rubriccategory.name').toLowerCase();
+      //      if (category_name == "impact") {
+      //        impact_criteria.push(crit);
+      //      }
+      //      //debugger
+      //      if ((index+1) == all_criteria_length) {
+      //        return impact_criteria;
+      //        //model.set("impact_criteria", impact_criteria);
+      //        //debugger
+      //      }
+      //    });
+//
+      //  
+      //});
+      //return impact_criteria;
       // returns null if the promise doesn't resolve immediately, or 
       // the calculated value if it's ready
-      return value;
-    }
-
+      // return value;
   }.property('all_criteria')
-  //var impact_crit = all_criteria.filterBy('')
 
+  //var impact_crit = all_criteria.filterBy('')
   //selectedEssays: function() {
   //  return this.get('content').filterBy('onboarding_is_selected', true);
   //}.property('content.@each.onboarding_is_selected'),
