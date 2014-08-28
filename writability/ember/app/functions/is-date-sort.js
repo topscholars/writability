cdb702b344e63be3f1b1962a0d830358e6f1362a
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-28 16:17:57 -0500

    Try to fix undefined is not a function?

--- writability/ember/app/functions/is-date-sort.js
+++ writability/ember/app/functions/is-date-sort.js
@@ -1,7 +1,7 @@
 export default function (a, b) {
     var regex = /\d{4}-\d{2}-\d{2}/;
 
-    if (a !== undefined && b !== undefined) {
+    if (a !== undefined && b !== undefined && a.match && b.match) {
     	return a.match(regex) && b.match(regex);
     }
 
