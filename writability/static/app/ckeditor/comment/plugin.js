e4c5676aba683335c6562135bd6c1acf63417013
Kirk Portas <kirkportas@gmail.com>
2014-08-28 00:36:15 -0400

    Fix enforcement of tag/annotation overlaps. (replaces ckeditor bookmark)

--- writability/static/app/ckeditor/comment/plugin.js
+++ writability/static/app/ckeditor/comment/plugin.js
@@ -22,21 +22,40 @@ CKEDITOR.plugins.add( 'comment', {
                 //}
                 // TODO -> check that something is already selected, or show a popup.
 
-                //Should be moved out..
+
+                // function getSelectionHtml(editor) {
+                //     var sel = editor.getSelection();
+                //     var ranges = sel.getRanges();
+                //     var el = new CKEDITOR.dom.element("div");
+                //     for (var i = 0, len = ranges.length; i < len; ++i) {
+                //         el.append(ranges[i].cloneContents());
+                //     }
+                //     return el.getHtml();
+                // }
+                // Should be moved out..
+                // var bookmarks = editor.getSelection().createBookmarks();
+                // editor.getSelection().selectBookmarks( bookmarks );
                 function getSelectionHtml(editor) {
+                    console.log('getSelectionHtml() !');
                     var sel = editor.getSelection();
+                    var bookmarks = sel.createBookmarks(); // creates bookmarks
                     var ranges = sel.getRanges();
                     var el = new CKEDITOR.dom.element("div");
                     for (var i = 0, len = ranges.length; i < len; ++i) {
                         el.append(ranges[i].cloneContents());
                     }
+                    editor.getSelection().selectBookmarks( bookmarks );
+                    console.log(el.getHtml);
                     return el.getHtml();
                 }
+
+                //var style = new CKEDITOR.style({attributes: {class: "annotation", id: "annotation-in-progress"}});
+                //editor.applyStyle(style);
 
                 //// This Breaks addcomment if called above function definition
                 // includes HTML within the selection, e.g. <span> tags
                 var selectedText = getSelectionHtml(editor);   
-                
+                //
                 // Enforce non-overlapping tags.
                 if (selectedText.indexOf("span") >= 0) {
                     alert('Sorry, but comments cannot overlap.'
