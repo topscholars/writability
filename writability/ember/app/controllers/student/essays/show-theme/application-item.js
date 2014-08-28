cf238894e642b406bdfafc5061c438969480eca6
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-28 14:31:21 -0500

    Update to match `not_selected` application essays #162

--- writability/ember/app/controllers/student/essays/show-theme/application-item.js
+++ writability/ember/app/controllers/student/essays/show-theme/application-item.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 	isSelected: Ember.computed.equal('state', 'selected'),
-	isUnselected: Ember.computed.equal('state', 'unselected'),
+	isUnselected: Ember.computed.equal('state', 'not_selected'),
 
 	actions: {
         select: function() {
